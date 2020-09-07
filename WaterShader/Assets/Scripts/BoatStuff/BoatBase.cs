703d088007bef3464b3fd80299173120f7e5c632
nicolasreiser <nico.reiser@gmail.com>
2020-09-07 17:01:05 +0200

    Managers are here

---
+++ WaterShader/Assets/Scripts/BoatStuff/BoatBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sets up the boat and its components
+/// </summary>
+public class BoatBase : MonoBehaviour
+{
+    [SerializeField] private BoatScriptable _boatScriptable;
+    [SerializeField] private NPCSpotsScript _nPCSpots;
+
+    public List<Transform> SpawnPoints = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Adds the spawnpoints to te list
+        for (int i = 0; i < _boatScriptable.NPCSpawnPointAmmount; i++)
+        {
+            SpawnPoints.Add(_nPCSpots._spots[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WaterShader/Assets/Scripts/Managers/MoneyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public static MoneyManager Instance;
+
+    public int Money;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddMoney(int ammount)
+    {
+        Money += ammount;
+    }
+
+    public bool CheckMoney(int ammount)
+    {
+        return ammount < Money ? true : false;
+    }
+}
