48fff1cab553935196ce720f24444314f9b6c443
Elias <caprisnoctis@googlemail.com>
2020-09-09 16:54:47 +0200

    added forwards movement and steering

--- WaterShader/Assets/Scripts/SimpleBoatInput.cs
+++ WaterShader/Assets/Scripts/SimpleBoatInput.cs
@@ -23,23 +23,22 @@ public class SimpleBoatInput : MonoBehaviour
 
         if (UI_InputDetect_Joystick.JoystickDirInDegrees > 0 && UI_InputDetect_Joystick.JoystickDirInDegrees < 180) //Accelerate
         {
-              Vector2 dir2D = Mathfs.GetUnitVectorByAngle(UI_InputDetect_Joystick.JoystickDirInDegrees * Mathf.Deg2Rad);
+            Vector2 dir2D = Mathfs.GetUnitVectorByAngle(UI_InputDetect_Joystick.JoystickDirInDegrees * Mathf.Deg2Rad);
             Vector3 dir = new Vector3(dir2D.x, 0, dir2D.y);
-            float xAxis = Mathfs.Remap(UI_InputDetect_Joystick.JoystickDirInDegrees, 0, 180, -1, 1); // X-Axis Input
-            Vector3 input = new Vector3(xAxis, 1, 0);
+          //  float xAxis = Mathfs.Remap(UI_InputDetect_Joystick.JoystickDirInDegrees, 0, 180, -1, 1); // X-Axis Input
+          //  Vector3 input = new Vector3(xAxis, 1, 0);
 
-            Debug.Log(" Degrees " + UI_InputDetect_Joystick.JoystickDirInDegrees + " Indi " + xAxis);
+         //   Debug.Log(" Degrees " + UI_InputDetect_Joystick.JoystickDirInDegrees + " Indi " + xAxis);
             
          //   Vector3 dir = new Vector3(0, UI_InputDetect_Joystick.JoystickDirInDegrees, 0);
             Quaternion delta = Quaternion.Euler(dir * Time.deltaTime);
-          //  _rb.MoveRotation(_rb.velocity);
 
             Quaternion dirQ = Quaternion.LookRotation(dir);
             Quaternion slerp = Quaternion.Slerp(transform.rotation, dirQ, dir.magnitude * _rSpeed * Time.deltaTime);
             _rb.MoveRotation(slerp);
 
        //     _rb.AddTorque(dir, ForceMode.Acceleration);
-           // _rb.AddForce(dir * _speed * Time.deltaTime, ForceMode.Acceleration);
+            _rb.AddForce(dir * _speed * Time.deltaTime, ForceMode.Acceleration);
             Debug.Log("Forward");
         }
         else // Slowdown
