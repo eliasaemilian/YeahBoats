9243bac0ae3883227cd3aed6ad9d2af45b30a2ac
Elias <caprisnoctis@googlemail.com>
2020-09-16 23:43:55 +0200

    ahaha HA : D kill me :D

---
+++ WaterShader/Assets/Scripts/Firebase/FirebaseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+
+public class FirebaseHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(continuationAction: task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
