ea56482e9ba1e35995016d0cbd75f5f10072339f
Elias <caprisnoctis@googlemail.com>
2020-09-16 22:18:12 +0200

    boat fix

--- WaterShader/Assets/Scripts/Boat/Movement and Physics/SimpleBoatInput.cs
+++ WaterShader/Assets/Scripts/Boat/Movement and Physics/SimpleBoatInput.cs
@@ -34,6 +34,8 @@ public class SimpleBoatInput : MonoBehaviour
 
         if (_reversedInput) dir = -dir;
 
+        dir = transform.TransformDirection(dir);
+
         if (UI_InputHandler.JoystickDirInDegrees > 0 && UI_InputHandler.JoystickDirInDegrees < 180) speed = _speed; //Accelerate
         else speed = -_reversingSpeed; //Reverse 
 
