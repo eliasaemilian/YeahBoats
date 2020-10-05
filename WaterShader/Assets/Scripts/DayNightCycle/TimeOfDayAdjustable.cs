dbf40c65110ad0d9358122710685bc9b689903a3
Elias <caprisnoctis@googlemail.com>
2020-10-05 20:17:54 +0200

    shapes ui fix

--- WaterShader/Assets/Scripts/DayNightCycle/TimeOfDayAdjustable.cs
+++ WaterShader/Assets/Scripts/DayNightCycle/TimeOfDayAdjustable.cs
@@ -2,34 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeOfDayAdjustable : MonoBehaviour
+public abstract class TimeOfDayAdjustable : MonoBehaviour
 {
-    private Light _light;
-
-    // put in SO eventually I gueess mayhaps
-    [SerializeField] private float _maxIntensity;
-
-    [SerializeField] private AnimationCurve _lightIntensity;
-    [SerializeField] private Gradient _lightColor;
-
     private TimeOfDayHandler _timeHandler;
 
     private void Start()
     {
-        _light = GetComponent<Light>();
         _timeHandler = FindObjectOfType<TimeOfDayHandler>();
         _timeHandler.TimeOfDayIsUpdated.AddListener(OnTimeOfDayValueChange);
 
-        if (_light == null) _light = GetComponentInChildren<Light>();
-        if (_light == null) Debug.LogError("There is no Light Attached to this GameObject, hecking do et D:<");
+        OnInitialize();
     }
 
-    public virtual void OnTimeOfDayValueChange(float newTime)
-    {
-        Debug.Log("Updating Lights");
+    public virtual void OnInitialize() { }
 
-        // Everything that needs to react to the time of day goes here
-        _light.intensity = _maxIntensity * _lightIntensity.Evaluate(newTime);
-        _light.color = _lightColor.Evaluate(newTime);
-    }
+    public virtual void OnTimeOfDayValueChange(float newTime) { }
 }
