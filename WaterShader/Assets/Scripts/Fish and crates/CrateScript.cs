0d64a5fe83f652326c413a254e6d3ac73e512f55
nicolasreiser <nico.reiser@gmail.com>
2020-10-21 13:03:50 +0200

    help

--- WaterShader/Assets/Scripts/Fish and crates/CrateScript.cs
+++ WaterShader/Assets/Scripts/Fish and crates/CrateScript.cs
@@ -6,19 +6,26 @@ public class CrateScript : TappableGameobject, IPooledObject
 {
     public CrateSpawner CS;
 
+    public override void OnStartInitialize()
+    {
+        base.OnStartInitialize();
+        //I need help dunno what to do
+        ObjectPooler.Instance.ReturnToPool("Crate", this.gameObject);
+
+    }
     public void OnInstantiation()
     {
-        throw new System.NotImplementedException();
+       // throw new System.NotImplementedException();
     }
 
     public void OnObjectDespawn()
     {
-        throw new System.NotImplementedException();
+       // throw new System.NotImplementedException();
     }
 
     public void OnObjectSpawn()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     private void OnTriggerEnter(Collider other)
