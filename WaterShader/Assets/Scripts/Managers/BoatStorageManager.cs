15142ff6503b3c9959353688516b76d82e2a1b00
nicolasreiser <nico.reiser@gmail.com>
2020-10-23 16:00:38 +0200

    Added, second boat and linekd boat shack UI

---
+++ WaterShader/Assets/Scripts/Managers/BoatStorageManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatStorageManager : MonoBehaviour
+{
+    [SerializeField] private LevelManager _lM;
+
+    public int BoatStorage;
+    private int _fishInStorage;
+
+    private void Start()
+    {
+        CalculateBoatStorage();
+    }
+
+
+    private void CalculateBoatStorage()
+    {
+        BoatStorage = 100 * (_lM.BoatStorageLevel + 2 * _lM.CurrentBoatLevel);
+    }
+
+
+}
