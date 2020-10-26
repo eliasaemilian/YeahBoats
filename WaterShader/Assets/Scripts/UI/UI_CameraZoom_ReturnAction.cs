85b369d4aae2cd73e83f9bdd3095ddb29261be6a
Elias <caprisnoctis@googlemail.com>
2020-10-26 12:57:44 +0100

    pub fix, return buttons

---
+++ WaterShader/Assets/Scripts/UI/UI_CameraZoom_ReturnAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_CameraZoom_ReturnAction : MonoBehaviour
+{
+    private TapEffect_CameraHandler _camHandler;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camHandler = FindObjectOfType<TapEffect_CameraHandler>();
+        if (_camHandler == null) Debug.LogError("Camera Zoom TapEffect needs a TapEffect CameraHandler in the Scene!");
+
+    }
+
+
+    public void OnReturnButtonClick()
+    {
+        _camHandler.OnCameraChange.Invoke(0);
+    }
+}
