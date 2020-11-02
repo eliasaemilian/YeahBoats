4bacf827b34e23fdfd6ecb3d02beb999db0047a4
Elias <caprisnoctis@googlemail.com>
2020-11-02 21:14:10 +0100

    icons

---
+++ WaterShader/Assets/Scripts/ScriptableObj/TutorialSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Tutorial/TutorialInstructionSet")]
+public class TutorialSet : ScriptableObject
+{
+    public List<TutorialInstruction> Instructions;
+}
+
+
+[Serializable]
+public class TutorialInstruction
+{
+    public string Instruction;
+    public Vector2 ScreenPos;
+}
---
+++ WaterShader/Assets/Scripts/UI/UI_TutorialPopUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_TutorialPopUp : TappableGameobject
+{
+    [SerializeField] private TutorialSet tutSet = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTutorialTriggered()
+    {
+        // get size somehow bleb
+        // put text into text field
+        // put at right position
+        // plop up background
+        // enable text
+        // start arrow movement
+    }
+
+    public override void OnTap(Touch touch, Vector3 pos)
+    {
+        base.OnTap(touch, pos);
+        // disbale tut box, move to next tut box
+    }
+
+    IEnumerator OpenTutorialPopUp()
+    {
+        yield return new WaitForSeconds(3f);
+    }
+}
