651f47c862237e2c28f3b02358b9350ce98814e5
Elias <caprisnoctis@googlemail.com>
2020-11-10 12:35:40 +0100

    added port popups

---
+++ WaterShader/Assets/Scripts/UI/UI_NotifPopUpsPort.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_NotifPopUpsPort : MonoBehaviour
+{
+    [SerializeField] private GameObject _popUpPrefab;
+
+    [SerializeField] private Camera _uiCamera;
+
+    private Transform[] _interactables;
+    private readonly List<Animation> _anims = new List<Animation>();
+
+
+    void Start()
+    {
+        _interactables = GetSceneInteractives();
+        if (_interactables != null)
+        {
+            PlaceNotifPopUpsOverInteractives();
+            CheckForNewUpdatesAvaliable();
+        }
+    }
+
+    /// <summary>
+    /// Collects all Children in Container tagged Interactives
+    /// </summary>
+    /// <returns></returns>
+    private Transform[] GetSceneInteractives()
+    {
+        GameObject parent = GameObject.FindGameObjectWithTag("Interactives");
+        List<Transform> children = new List<Transform>();
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {           
+            if (parent.transform.GetChild(i).Find("NotifPos")) children.Add(parent.transform.GetChild(i).Find("NotifPos"));
+        }
+        Transform[] fetched = children.ToArray();
+        return fetched;
+    }
+
+    /// <summary>
+    /// Place UI Elements over Interactive Models
+    /// </summary>
+    private void PlaceNotifPopUpsOverInteractives()
+    {
+        for (int i = 0; i < _interactables.Length; i++)
+        {
+            Vector3 screenPos = Camera.main.WorldToViewportPoint(_interactables[i].position);
+            screenPos.z = transform.position.z;
+            GameObject popUp = Instantiate(_popUpPrefab, transform);
+            popUp.transform.position = _uiCamera.ViewportToWorldPoint(screenPos);
+            if (popUp.GetComponentInChildren<Animation>() != null) _anims.Add(popUp.GetComponentInChildren<Animation>());
+        }
+    }
+
+    /// <summary>
+    /// For each Store if they have any new Updates avaliable, play notif animation
+    /// </summary>
+    private void CheckForNewUpdatesAvaliable()
+    {
+        // for each Interactive if Update Avaliable play anim else pause anim
+
+
+        // HENLO
+
+
+        // HERE
+
+
+        // WHOOP
+    }
+}
