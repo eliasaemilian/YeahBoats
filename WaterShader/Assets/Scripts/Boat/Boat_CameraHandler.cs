c23a358e43d292d9f834c2cc8f9c414929654e75
Elias <caprisnoctis@googlemail.com>
2020-11-23 16:51:01 +0100

    camera added focus points for bigger boats

--- WaterShader/Assets/Scripts/Boat/Boat_CameraHandler.cs
+++ WaterShader/Assets/Scripts/Boat/Boat_CameraHandler.cs
@@ -9,6 +9,10 @@ public class Boat_CameraHandler : MonoBehaviour
     private UI_JoystickHandler _joystickHandler;
     private CinemachineStateDrivenCamera _cineCam;
 
+    private Transform _cineCamFocusPoint, _cineCamFocusPointFishing;
+
+    private string _tagFocusPoint = "CamFocusPoint", _tagFocusPointFishing = "CamFocusPointFishing";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +21,7 @@ public class Boat_CameraHandler : MonoBehaviour
         if (UI_JoystickHandler.JoystickStateChanged == null) Debug.LogError("Boat CameraHandler needs a Joystick in the Scene. Add a JoystickHandler or delete CameraHandler from Boat");
         UI_JoystickHandler.JoystickStateChanged.AddListener(On_UI_InputDetect_Joystick_JoystickEventChanged);
 
-        _cineCam = FindObjectOfType<CinemachineStateDrivenCamera>();
-        if (_cineCam == null) Debug.Log("Frick D:");
-        _cineCam.Follow = transform;
-        _cineCam.LookAt = transform;
-        _cineCam.m_AnimatedTarget = _anim;
-
-
-        if (LevelManager.Instance.CurrentBoatLevel >= 3)
-        {
-            CinemachineVirtualCamera[] vCams = FindObjectsOfType<CinemachineVirtualCamera>();
-            for (int i = 0; i < vCams.Length; i++)
-            {
-                vCams[i].m_Lens.FieldOfView = 60f;
-            }
-        }
+        InitCinemachineCameraSetup();
     }
 
     /// <summary>
@@ -42,10 +32,57 @@ public class Boat_CameraHandler : MonoBehaviour
         if (_joystickHandler.JoystickStateClosed)
         {
             _anim.SetBool("State_Fishing", true);
+
+            SetCameraFocusPoint(_cineCamFocusPointFishing);
+
         }
         else
         {
             _anim.SetBool("State_Fishing", false);
+
+            SetCameraFocusPoint(_cineCamFocusPoint);
+
+        }
+    }
+
+    private Transform CollectCamFocusPoint(string tag)
+    {
+        Transform[] children = GetComponentsInChildren<Transform>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (children[i].CompareTag(tag)) return children[i];
+        }
+
+        return null;
+    }
+
+    private void SetCameraFocusPoint(Transform focus)
+    {
+        if (_cineCam == null) Debug.Log("Frick D:");
+
+        _cineCam.Follow = focus;
+        _cineCam.LookAt = focus;
+    }
+
+    private void InitCinemachineCameraSetup()
+    {
+        _cineCam = FindObjectOfType<CinemachineStateDrivenCamera>();
+
+        _cineCamFocusPoint = transform; _cineCamFocusPointFishing = transform;
+        if (CollectCamFocusPoint(_tagFocusPoint) != null) _cineCamFocusPoint = CollectCamFocusPoint(_tagFocusPoint);
+        if (CollectCamFocusPoint(_tagFocusPointFishing) != null) _cineCamFocusPointFishing = CollectCamFocusPoint(_tagFocusPointFishing);
+
+        SetCameraFocusPoint(_cineCamFocusPoint);
+        _cineCam.m_AnimatedTarget = _anim;
+
+
+        if (LevelManager.Instance.CurrentBoatLevel >= 3)
+        {
+            CinemachineVirtualCamera[] vCams = FindObjectsOfType<CinemachineVirtualCamera>();
+            for (int i = 0; i < vCams.Length; i++)
+            {
+                vCams[i].m_Lens.FieldOfView = 60f;
+            }
         }
     }
 }
